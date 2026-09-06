b54b93e1bb4370d10d629d35e8eab96ee0e6be99
GeraKTT <geraktt@gmail.com>
2026-09-06 05:15:17 -0500

    primer programa

---
+++ DatosyAlgoritmos.java
@@ -0,0 +1,14 @@
+public class DatosyAlgoritmos {
+
+    public static void main(String[] args) {
+    
+        int[] notas = {10,15,20,8};
+
+        int suma = notas[0] + notas[1] + notas[2] + notas[3];
+        double promedio = suma / 4;
+
+        boolean aprobado = promedio >= 13;
+
+        System.out.println("Aprovado: " + aprobado);
+    }
+}
